5714e68483dc0767d8ca94dae47152f3345edcfc
Teo-Ay <teoayre@gmail.com>
2025-11-16 21:54:19 +0100

    Add files via upload

---
+++ Assignment 1_5 Teodor Ayre/scripts.js
@@ -0,0 +1,15 @@
+let vip = false;   
+let age = 17;      
+
+
+if (vip === true) {
+    alert("Welcome in my best VIP customer :)");
+}
+
+else if (age >= 18) {
+    alert("Access granted");
+} 
+else {
+    alert("Access denied");
+}
+
